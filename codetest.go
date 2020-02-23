f474293d5ca6893f92d1de342f879b0555a45791
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-02-23 22:51:46 +0700

    Update codetest.go

--- codetest.go
+++ codetest.go
@@ -1,10 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func getDrives() (r []string) {
 	fmt.Print("input : ")
 	var get string
 	fmt.Scan(&get)
-
+	f, err := os.Open(string(get) + ":\\")
+	if err == nil {
+		drive := string(get) + ":/"
+		r = append(r, drive)
+	}
 }
